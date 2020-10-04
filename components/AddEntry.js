76a2b0f1933719a95523504f7112b39f5eb44bc2
hannya <hanny.aly@gmail.com>
2020-10-03 20:30:26 -0400

    feat: Add AddEntry

---
+++ components/AddEntry.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react'
+import {View, Text } from 'react-native'
+import { getMetricMetaInfo } from '../utils/helpers'
+
+export default class AddEntry extends Component {
+
+    state = {
+        run: 0,
+        bike: 0,
+        swim: 0,
+        eat: 0,
+        sleep:0
+    }
+
+    increment = (metric) => {
+        const {max, step} = getMetricMetaInfo(metric)
+
+        this.setState((prevState) => {
+            const count = prevState[metric] + step
+
+            return {
+                ...prevState,
+                [metric]: count > max ? max : count
+            }
+        })
+    }
+
+    deccrement = (metric) => {
+        const { step } = getMetricMetaInfo(metric)
+
+        this.setState((prevState) => {
+            const count = prevState[metric] - step
+
+            return {
+                ...prevState,
+                [metric]: count < 0 ? 0 : count
+            }
+        })
+    }
+
+    slide = (metric, value) => {
+        this.setState({
+            [metric]: value
+        })
+    }
+
+    render() {
+        return (
+            <View>
+                {getMetricMetaInfo('bike').getIcon() }
+            </View>
+        )
+    }
+}
