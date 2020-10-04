0343916e4825d014dea48c8f25836204ab0763f5
hannya <hanny.aly@gmail.com>
2020-10-04 01:35:50 -0400

    feat: Filled out code for Slider

--- components/FitSlider.js
+++ components/FitSlider.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import {  View, Text } from 'react-native'
+import Slider from '@react-native-community/slider'
 
-export default function FitSlider() {
+export default function FitSlider({max, unit, step, value, onChange}) {
     return (
         <View>
-            <Text>
-                Slider
-            </Text>
+            <Slider
+                step={step}
+                value={value}
+                maximumValue={max}
+                minimumValue={0}
+                unit={unit}
+                onValueChange={onChange}
+            />
+            <View>
+                <Text>{value}</Text>
+                <Text>{unit}</Text>
+            </View>
         </View>
     )
 }
